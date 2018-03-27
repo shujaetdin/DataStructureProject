36a0877260e02e7d17655fc83014e1eaa86b91f3
shujaetdin <shujaetdin@gmail.com>
2018-03-27 09:13:32 -0600

    created new class called BinarytreeTester

---
+++ DataStructureProject/Testers/BinaryTreeTester.cpp
@@ -0,0 +1,16 @@
+//
+//  BinaryTreeTester.cpp
+//  DataStructureProject
+//
+//  Created by Din, Shujaet on 3/27/18.
+//  Copyright © 2018 Din, Shujaet. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+
+
+#include "BinaryTreeTester.hpp"
+#include "../Model/Nodes/BinaryTreeNode.hpp"
+#include <iostream>
+
+
---
+++ DataStructureProject/Testers/BinaryTreeTester.hpp
@@ -0,0 +1,14 @@
+//
+//  BinaryTreeTester.hpp
+//  DataStructureProject
+//
+//  Created by Din, Shujaet on 3/27/18.
+//  Copyright © 2018 Din, Shujaet. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include <stdio.h>
+
+#endif /* BinaryTreeTester_hpp */
